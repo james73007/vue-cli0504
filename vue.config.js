e38ee26ce211cf8727c918973e5c6cb4b5a73a6b
james232323 <roger73007@gmail.com>
2022-05-04 22:16:43 +0800

    fix gitignore

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/vue-cli0504/'
-      : '/'
-  }
-  
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-cli0504/dist' : '/',
+};
